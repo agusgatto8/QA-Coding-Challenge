2f50d3f7d097f9df60283bc7288c1f5acb6a5274
Agustin <agustin.gatto@radiumrocket.com>
2023-08-04 17:17:16 -0300

    Add collision and unit tests

--- coding-challenge-qa-uy/js/script.js
+++ coding-challenge-qa-uy/js/script.js
@@ -1,41 +1,50 @@
 var pacman;
 var ghost;
+var pacmanX = 0;
+var pacmanY = 0;
+var originalPosition = { x: 0, y: 0 };
 
-window.addEventListener('DOMContentLoaded', function(){
-    pacman = document.getElementById('pacman');
-    ghost = document.getElementById('ghost');
+function pacmanResetPosition() {
+	pacmanX = originalPosition.x;
+	pacmanY = originalPosition.y;
+	pacman.style.left = pacmanX + "px";
+	pacman.style.top = pacmanY + "px";
+}
 
-    var pacmanX = 0;
-    var pacmanY = 0;
+window.addEventListener("DOMContentLoaded", function () {
+	pacman = document.getElementById("pacman");
+	ghost = document.getElementById("ghost");
 
-    window.addEventListener('keydown', function(event) {
+	originalPosition.x = pacmanX;
+	originalPosition.y = pacmanY;
 
-        if (event.key === "ArrowLeft") {
-            pacmanX -= 5;
-        }
+	window.addEventListener("keydown", function (event) {
+		switch (event.key) {
+			case "ArrowLeft":
+				pacmanX -= 5;
+				break;
+			case "ArrowRight":
+				pacmanX += 5;
+				break;
+			case "ArrowUp":
+				pacmanY -= 5;
+				break;
+			case "ArrowDown":
+				pacmanY += 5;
+				break;
+		}
 
-        else if (event.key === "ArrowRight") {
-            pacmanX += 5;
-        }
+		pacman.style.left = pacmanX + "px";
+		pacman.style.top = pacmanY + "px";
 
-        else if (event.key === "ArrowUp") {
-            pacmanY -= 5;
-        }
-
-        else if (event.key === "ArrowDown") {
-            pacmanY += 5;
-        }
-
-        pacman.style.left = pacmanX + 'px';
-        pacman.style.top = pacmanY + 'px';
-
-        if (
-            pacmanX + pacman.offsetWidth > ghost.offsetLeft &&
-            pacmanX < ghost.offsetLeft + ghost.offsetWidth &&
-            pacmanY + pacman.offsetHeight > ghost.offsetTop &&
-            pacmanY < ghost.offsetTop + ghost.offsetHeight
-        ) {
-            alert("Game over!");
-        }
-    });
+		if (
+			pacmanX + pacman.offsetWidth > ghost.offsetLeft &&
+			pacmanX < ghost.offsetLeft + ghost.offsetWidth &&
+			pacmanY + pacman.offsetHeight > ghost.offsetTop &&
+			pacmanY < ghost.offsetTop + ghost.offsetHeight
+		) {
+			alert("Game over!");
+            pacmanResetPosition();
+		}
+	});
 });
---
+++ coding-challenge-qa-uy/tests.md
@@ -0,0 +1,6 @@
+# Pruebas unitarias
+
+1. **Pruebas de movimientos:** Realizar pruebas de movimientos creando eventos con el teclado para asegurar que "Pacman" se mueve correctamente.
+2. **Pruebas de colicion:** Una vez se cumpla el resultado esperado del punto numero uno, intentar que "Pacman" colisione con "Fantasmita" y chequear que se desplega la alerta con el mensaje descriptivo correspondiente.
+3. **Pruebas al restablecimiento de posicion:** Realizar pruebas a la funcion "pacmanResetPosition" para asegurarnos que una vez que sea ejecutada la posicion de "Pacman" sea la inicial.
+4. **Pruebas de movimiento:** Volver a ejecutar el punto numero uno y verificar que una vez se restablezca la posicion de "Pacman", los movimientos sigan funcionando correctamente.
